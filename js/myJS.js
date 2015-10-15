d0b4a885809c0e15411d72b68f5f72fb92a4ec5e
Angela Niu <angela50334@gmail.com>
2015-10-14 22:22:10 -0400

    Minor fixes

--- js/myJS.js
+++ js/myJS.js
@@ -38,24 +38,24 @@ $(document).ready(function(){
 
 //This section is for the smooth scrolling effect that happens when you click on a subject on the home page
 $(document).ready(function() {
-  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
+  $('a[href*=#]').each(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top;
 
-         $(this).click(function() {
+         $(this).click(function() {
             $("#subjects li a").removeClass("active");
             $(this).addClass('active');
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           return false;
-         });
-      }
-    }
-  });
+           $('html, body').animate({scrollTop: targetOffset}, 1000);
+           return false;
+         });
+      }
+    }
+  });
 
 });
 
@@ -68,6 +68,3 @@ $(document).ready(function() {
 		$(this).removeClass('flip');
 	});
 });
-
-
-	
